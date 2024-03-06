bd1c835591975e4af263584c81cb5e827fcb881c
guhge@github.com <mburlacenko798@gmail.com>
2024-03-06 21:46:46 +0300

    Text is done

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ let token_expire_time, senderName
 
 const user_id = '294610886'
 const client_id = 'jIC1ZsbUkDO_E2FymKEd'
-const client_secret = 'NceYZWK68aCYctPihLoBnq_7iXOQ0Jfq4nVgx5hq'
 
 // Получение access_token
 async function getAccessToken(url) {
@@ -18,7 +17,8 @@ async function getAccessToken(url) {
     .then(response => response.json())
     .then(data => {
       access_token = data.access_token;
-      console.log(access_token)
+      token_expire_time = data.expires_in;
+      console.log('Авторизация прошла успешно\n\n' + access_token + '\n')
     });
     return access_token;
     // access_token = data.access_token;
@@ -80,15 +80,31 @@ async function getReviews(url, access_token) {
 export async function Home() {
   let access_token = await getAccessToken('https://api.avito.ru/token');
   let reviews = await getReviews('https://api.avito.ru/ratings/v1/reviews', access_token);
-
+  let date = new Date(reviews.reviews[0].createdAt * 1000);
+  let stage = reviews.reviews[0].stage;
+  let staged;
+  switch (stage){
+    case 'done':
+      staged = 'Сделка состоялась';
+      break;
+    case 'fell_through':
+      staged = 'Сделка сорвалaсь';
+      break;
+    case 'not_agree':
+      staged = 'Не договорились';
+      break;
+    case 'not_communicate':
+      staged = 'Не общались';
+      break;
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div class="review">
+      <div className="review">
       <br/><span id="name">{reviews.reviews[0].sender.name}</span>
-      <br/><span class="date">12 апреля 2021</span>
-      <br/><span class="status"><span class="text">★★★★☆</span> Сделка состоялась: "Кот"</span>
-      <br/><b>Комментарий</b><br/>Очень хороший отзыв о продукте или услуге.
+      <br/><span className="date">{date.toLocaleDateString("ru-GB")}</span>
+      <br/><span className="status"><span className="text">★★★★☆</span> {staged}: "{reviews.reviews[0].item.title}"</span>
+      <br/><b>Комментарий</b><br/>{reviews.reviews[0].text}
       </div>
     </main>
   );
